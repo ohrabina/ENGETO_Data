8e2153c28abed0e0225cbed8ad75f008dafc3498
Ondrej Hrabina <hrabina.ondra@gmail.com>
2023-04-01 19:06:36 +0200

    Creation of tables for prices and vages

--- SQL_Project/t_ondrej_hrabina_project_SQL_primary_final.sql
+++ SQL_Project/t_ondrej_hrabina_project_SQL_primary_final.sql
@@ -4,18 +4,18 @@ SELECT
 	ROUND(AVG(cp.value), 2) AS avg_price,
 	cpc.price_value AS unit_value,
 	cpc.price_unit AS unit,
-	date_format(cp.date_from, '%Y') AS avg_year,
+	date_format(cp.date_from, '%Y') AS price_year,
 	quarter(date_from) AS qtr
 FROM czechia_price AS cp 
 JOIN czechia_price_category AS cpc ON cp.category_code = cpc.code
 JOIN czechia_region AS cr ON cp.region_code = cr.code
 GROUP BY 
 	name,
-	avg_year,
+	price_year,
 	qtr
 ORDER BY 
 	cpc.name,
-	avg_year,
+	price_year,
 	qtr
 )
 
@@ -26,12 +26,13 @@ SELECT
 	payroll_year,
 	payroll_quarter,
 	cpib.name AS branch,
-	cpvt.name AS TYPE
+	cpvt.name AS job_type
 FROM czechia_payroll cp
 JOIN czechia_payroll_industry_branch cpib ON cp.industry_branch_code = cpib.code 
 JOIN czechia_payroll_calculation cpc ON cp.unit_code = cpc.code 
 JOIN czechia_payroll_value_type cpvt ON cp.value_type_code = cpvt.code
 JOIN czechia_payroll_unit cpu ON cp.unit_code = cpu.code
+WHERE cpvt.code = '5958'
 GROUP BY 
 	branch,
 	payroll_year, 
@@ -41,3 +42,21 @@ ORDER BY
 	payroll_year,
 	payroll_quarter
 )
+
+SELECT 
+	tp.name,
+	tp.avg_price,
+	tp.unit_value,
+	tp.unit,
+	tp.price_year,
+	tp.qtr,
+	tv.avg_pay,
+	tv.currency,
+	tv.branch,
+	e.GDP
+FROM temp_prices AS tp
+JOIN temp_vages AS tv ON tp.price_year = tv.payroll_year AND tp.qtr = tv.payroll_quarter
+JOIN economies e ON tp.price_year = e.`year`
+WHERE e.country = 'Czech Republic'
+
+
