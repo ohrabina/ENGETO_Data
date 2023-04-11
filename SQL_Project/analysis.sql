10fb835e4fbc011abbd352ad419f71394e3c386d
Ondrej Hrabina <hrabina.ondra@gmail.com>
2023-04-11 20:50:18 +0200

    Data for 1st question added

--- SQL_Project/analysis.sql
+++ SQL_Project/analysis.sql
@@ -1,7 +1,21 @@
 -- First question - does avg_pay in specific Branch rise during investigated years?
-
+CREATE TEMPORARY TABLE min_max(
 SELECT 
-	DISTINCT price_year AS pay_year,
-	tpf.avg_pay,
-	branch
+	branch,
+	MIN(avg_pay) AS min_pay,
+	MAX(avg_pay) AS max_pay
 FROM t_ondrej_hrabina_project_sql_primary_final AS tpf
+GROUP BY branch
+)
+
+SELECT
+	min_max.branch,
+	ROUND(min_max.min_pay) AS min_pay,
+	ROUND(min_max.max_pay) AS max_pay,
+	tpf.price_year AS min_year,
+	tpf2.price_year AS max_year
+FROM min_max
+JOIN t_ondrej_hrabina_project_sql_primary_final AS tpf ON min_max.min_pay = tpf.avg_pay
+JOIN t_ondrej_hrabina_project_sql_primary_final AS tpf2 ON min_max.max_pay = tpf2.avg_pay
+GROUP BY 
+	branch
