8922c7249a3afc84ebb971d75bde9802700aa133
Ondrej Hrabina <hrabina.ondra@gmail.com>
2023-04-16 17:47:25 +0200

    Additon of supporting data for senond question

--- SQL_Project/analysis.sql
+++ SQL_Project/analysis.sql
@@ -19,3 +19,26 @@ JOIN t_ondrej_hrabina_project_sql_primary_final AS tpf ON min_max.min_pay = tpf.
 JOIN t_ondrej_hrabina_project_sql_primary_final AS tpf2 ON min_max.max_pay = tpf2.avg_pay
 GROUP BY 
 	branch
+
+-- Second question - how many l/kg of milk/bread one can buy in first and last comparable period?
+SELECT 
+	name,
+	avg_price,
+	unit_value,
+	unit,
+	price_year,
+	round(AVG(avg_pay)) AS pay_per_year,
+	round(round(AVG(avg_pay))/avg_price) AS amount,
+	unit AS unit_2
+FROM t_ondrej_hrabina_project_sql_primary_final AS tpf
+WHERE 
+	(name = 'Chléb konzumní kmínový' or name = 'Mléko polotučné pasterované') AND 
+	(price_year = '2006' OR price_year = '2018')
+GROUP BY
+	name,
+	price_year 
+
+-- Third question - Which food categhory has the lowest price increase(year price rise in %)?
+-- grow rate = ((last/first)^1/n) - 1
+	
+	
